6330af6c0e608c44bc275b3510bbff45ef064832
junaid <jsultanwork@gmail.com>
2026-03-19 03:10:10 +0500

    chapter-2, code edit, number guessing code in main.rs

--- chapter-2/src/main.rs
+++ chapter-2/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number: ");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("you guessed: {guess}");
 }
