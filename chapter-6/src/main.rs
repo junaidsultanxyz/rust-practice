b6116d4c78d449abe3a0894a3a633c64bb35ba91
Junaid Sultan <jsultanwork@gmail.com>
2026-04-07 00:01:44 +0500

    chapter-6, 6.3, completed with if let else control

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -44,7 +44,13 @@ impl Message {
 
 fn main() {
     println!("## Chapter 6");
+
+    /* ######################################################################### */
+    /* 6.1                                                                       */
+    /* ######################################################################### */
     println!("#### 6.1");
+
+
     let pc_ip_local = IpAddressStruct {
         kind: IpKind::V4,
         address: String::from("192.168.100.10")
@@ -116,7 +122,7 @@ fn main() {
         Quarter(UsState)
     }
 
-    fn value_in_cents(coin: Coin) -> u8 {
+    fn value_in_cents(coin: &Coin) -> u8 {
         match coin {
             Coin::Penny => 1,
             Coin::Nickel => 5,
@@ -135,11 +141,11 @@ fn main() {
     let alaska_quarter = Coin::Quarter(UsState::Alaska);
     
 
-    println!("penny = {}", value_in_cents(penny));
-    println!("nickel = {}", value_in_cents(nickel));
-    println!("dime = {}", value_in_cents(dime));
-    println!("alabama_quarter = {}", value_in_cents(alabama_quarter));
-    println!("alaska_quarter = {}", value_in_cents(alaska_quarter));
+    println!("penny = {}", value_in_cents(&penny));
+    println!("nickel = {}", value_in_cents(&nickel));
+    println!("dime = {}", value_in_cents(&dime));
+    println!("alabama_quarter = {}", value_in_cents(&alabama_quarter));
+    println!("alaska_quarter = {}", value_in_cents(&alaska_quarter));
 
     let dice_roll = 3;
     match dice_roll {
@@ -147,4 +153,55 @@ fn main() {
         7 => println!("removing hat :("),
         _ => () // nothing
     }
+
+
+    /* ######################################################################### */
+    /* 6.3                                                                       */
+    /* ######################################################################### */
+    println!("\n#### 6.3");
+
+
+    let mut config_max = Some(3u8);
+
+    match config_max {
+        Some(max) => println!("max configured = {max}"),
+        _ => () // boilerplate if not needed.
+    }
+
+    /*
+        instead, we can use if let.
+    */
+    config_max = None;
+    if let Some(max) = config_max {
+        println!("max configured = {max}");
+    }
+    else {
+        println!("max not configured");
+    }
+
+    impl UsState {
+        fn existed_in(&self, year: u16) -> bool {
+            match self {
+                UsState::Alabama => year >= 1819,
+                UsState::Alaska => year >= 1959,
+            }
+        }
+    }
+
+    fn describe_state_quarter(coin: &Coin) -> Option<String> {
+        let state = if let Coin::Quarter(state) = coin {
+            state
+        } else {
+            return None;
+        };
+
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relatively new."))
+        }
+    }
+
+    println!("{}", describe_state_quarter(&alabama_quarter).unwrap());
+    println!("{}", describe_state_quarter(&alaska_quarter).unwrap());
 }
