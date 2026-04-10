298ac9677a86d5ec8150723d2952a629af1ca421
Junaid Sultan <jsultanwork@gmail.com>
2026-04-11 02:58:40 +0500

    chapter-7, 7.2, creating, using and definiting visibility of modules

--- chapter-7/src/main.rs
+++ chapter-7/src/main.rs
@@ -1,3 +1,16 @@
+use crate::garden::vegetables::Asparagus; // cannot find it unless we add the module and it is visible
+
+// inline module
+// pub mod garden {
+//     pub mod vegetables {
+//         #[derive(Debug)]
+//         pub struct Asparagus;
+//     }
+// }
+
+mod garden; // includes all the public modules inside garden.
+// this will include: [ garden( vegetables(Asparagus) ) ]
 fn main() {
-    println!("Hello, world!");
+    let plant = Asparagus;
+    println!("I'm growing {plant:?}!");
 }
