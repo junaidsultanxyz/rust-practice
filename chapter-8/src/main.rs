e265333127dbcf335f946076bc41bc9f5b81d7e1
junaid <jsultanwork@gmail.com>
2026-04-22 22:33:13 +0500

    chapter-8, main, main file updated for 8.1/2/3 modules

--- chapter-8/src/main.rs
+++ chapter-8/src/main.rs
@@ -2,15 +2,17 @@ mod vectors;
 mod utf8;
 mod hashmaps;
 
-use vectors::part1;
+use crate::hashmaps::part3;
+use crate::vectors::part1;
+use crate::utf8::part2;
 
 fn main() {
     println!("#### PART 1 : VECTORS ####");
     part1();
 
     println!("#### PART 2 : UTF8 String ####");
-    part1();
+    part2();
 
     println!("#### PART 3 : HASHMAPS ####");
-    part1();
+    part3();
 }
